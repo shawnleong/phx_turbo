b74f496364230d377f605e84a8076e710e26f838
Shawn Leong <shawn_leong@icloud.com>
2021-11-15 17:01:19 +0800

    Add turbo

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,8 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+import * as Turbo from "@hotwired/turbo"
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
@@ -34,11 +36,14 @@ topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
-// connect if there are any LiveViews on the page
-liveSocket.connect()
+document.addEventListener("turbo:load", () => {
+  liveSocket.destroyAllViews()
+  // connect if there are any LiveViews on the page
+  liveSocket.connect()
 
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
+  // expose liveSocket on window for web console debug logs and latency simulation:
+  // >> liveSocket.enableDebug()
+  // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+  // >> liveSocket.disableLatencySim()
+  window.liveSocket = liveSocket
+})
--- lib/turbo_web/components/link.ex
+++ lib/turbo_web/components/link.ex
@@ -5,7 +5,7 @@ defmodule TurboWeb.LinkComponent do
     ~H"""
     <div>
       <%= for module <- [TurboWeb.Live1, TurboWeb.Live2, TurboWeb.Live3] do %>
-        <%= link "#{module} (link)", to: Routes.live_path(@socket, module) %>
+        <%= link "#{module} (link)", to: Routes.live_url(@socket, module) %>
         <%= live_redirect "#{module} (live_redirect)", to: Routes.live_path(@socket, module) %>
         <%= live_patch "#{module} (live_patch)", to: Routes.live_path(@socket, module) %>
         <hr/>
